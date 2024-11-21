b59f815cb4331d926fa177522190c31435d6b5fe
neco8 <shogibeshow@gmail.com>
2024-11-21 11:31:53 +0900

    add: custom-backend-taskを追加

---
+++ neco8-log/app/BackendTasks/ConvertMarkdownFiles.elm
@@ -0,0 +1,56 @@
+module BackendTasks.ConvertMarkdownFiles exposing (Rule, run, ConversionResult)
+
+import BackendTask exposing (BackendTask)
+import BackendTask.Custom
+import FatalError exposing (FatalError)
+import Json.Decode as Decode
+import Json.Encode as Encode
+
+
+type alias Rule =
+    { before : String
+    , after : String
+    }
+
+
+type alias ConversionResult =
+    { success : Bool
+    , source : String
+    , destination : Maybe String
+    , error : Maybe String
+    }
+
+
+resultDecoder : Decode.Decoder ConversionResult
+resultDecoder =
+    Decode.map4 ConversionResult
+        (Decode.field "success" Decode.bool)
+        (Decode.field "source" Decode.string)
+        (Decode.maybe (Decode.field "destination" Decode.string))
+        (Decode.maybe (Decode.field "error" Decode.string))
+
+
+encodeRule : Rule -> Encode.Value
+encodeRule rule =
+    Encode.object
+        [ ( "before", Encode.string rule.before )
+        , ( "after", Encode.string rule.after )
+        ]
+
+
+run :
+    { sourceDir : String
+    , destDir : String
+    , rules : List Rule
+    }
+    -> BackendTask FatalError (List ConversionResult)
+run params =
+    BackendTask.Custom.run "convertMarkdownFiles"
+        (Encode.object
+            [ ( "sourceDir", Encode.string params.sourceDir )
+            , ( "destDir", Encode.string params.destDir )
+            , ( "rules", Encode.list encodeRule params.rules )
+            ]
+        )
+        (Decode.list resultDecoder)
+        |> BackendTask.mapError .fatal
--- neco8-log/app/Site.elm
+++ neco8-log/app/Site.elm
@@ -1,10 +1,10 @@
 module Site exposing (config)
 
 import BackendTask exposing (BackendTask)
+import BackendTasks.ConvertMarkdownFiles as ConvertMarkdownFiles
 import FatalError exposing (FatalError)
 import Head
 import SiteConfig exposing (SiteConfig)
-
 
 config : SiteConfig
 config =
@@ -12,10 +12,26 @@ config =
     , head = head
     }
 
+-- ファイル変換を行うBackendTask
+convertFiles : BackendTask FatalError (List ConvertMarkdownFiles.ConversionResult)
+convertFiles =
+    ConvertMarkdownFiles.run
+        { sourceDir = "obsidian"
+        , destDir = "content/blog"
+        , rules =
+            [ { before = "fileName", after = "title" }
+            , { before = "path", after = "fileName" }
+            ]
+        }
 
+-- head タグを生成するBackendTask
 head : BackendTask FatalError (List Head.Tag)
 head =
-    [ Head.metaName "viewport" (Head.raw "width=device-width,initial-scale=1")
-    , Head.sitemapLink "/sitemap.xml"
-    ]
-        |> BackendTask.succeed
+    convertFiles
+        |> BackendTask.andThen
+            (\_ ->
+                BackendTask.succeed
+                    [ Head.metaName "viewport" (Head.raw "width=device-width,initial-scale=1")
+                    , Head.sitemapLink "/sitemap.xml"
+                    ]
+            )
